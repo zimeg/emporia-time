31dcdca4840702a96541f7ac0a39e319c25ea6f1
Ethan Zimbelman <ethan.zimbelman@me.com>
2022-12-20 20:08:58 -0600

    exit survey on interrupt

--- survey.go
+++ survey.go
@@ -3,9 +3,13 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/AlecAivazis/survey/v2"
+	"github.com/AlecAivazis/survey/v2/terminal"
 )
+
+var TERMINAL_INTERRUPT = 1
 
 // collectCredentials prompts for an Emporia username and password
 func collectCredentials() (string, string) {
@@ -13,8 +17,15 @@ func collectCredentials() (string, string) {
 	var password string
 
 	fmt.Printf("Enter your Emporia credentials <https://web.emporiaenergy.com/>\n")
-	survey.AskOne(&survey.Input{Message: "Username"}, &username)
-	survey.AskOne(&survey.Password{Message: "Password"}, &password)
+	err := survey.AskOne(&survey.Input{Message: "Username"}, &username)
+	if err == terminal.InterruptErr {
+		os.Exit(TERMINAL_INTERRUPT)
+	}
+
+	err = survey.AskOne(&survey.Password{Message: "Password"}, &password)
+	if err == terminal.InterruptErr {
+		os.Exit(TERMINAL_INTERRUPT)
+	}
 
 	return username, password
 }
@@ -41,7 +52,11 @@ func selectDevice(devices []EmporiaDevice) string {
 			return fmt.Sprintf("#%d", gids[index])
 		},
 	}
-	survey.AskOne(prompt, &selected)
+
+	err := survey.AskOne(prompt, &selected)
+	if err == terminal.InterruptErr {
+		os.Exit(TERMINAL_INTERRUPT)
+	}
 
 	var gid int
 	for index, val := range names {
