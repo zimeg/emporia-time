1dc3d14ce780ef9b7730032b4ffc2cdab6974330
Ethan Zimbelman <ethan.zimbelman@me.com>
2023-06-17 20:18:51 -0700

    move status func to end of file

--- pkg/emporia/request.go
+++ pkg/emporia/request.go
@@ -120,22 +120,6 @@ func (emp *Emporia) getEnergyUsage(params url.Values) ([]float64, error) {
 	return emp.Resp.UsageList, nil
 }
 
-// EmporiaStatus returns if the Emporia API is available
-func EmporiaStatus() (bool, error) {
-
-	// https://github.com/magico13/PyEmVue/blob/master/api_docs.md#detection-of-maintenance
-	EmporiaStatusURL := "https://s3.amazonaws.com/com.emporiaenergy.manual.ota/maintenance/maintenance.json"
-
-	resp, err := http.Get(EmporiaStatusURL)
-	if err != nil {
-		return false, err
-	}
-	defer resp.Body.Close()
-
-	status := resp.StatusCode == 403
-	return status, nil
-}
-
 // getAvailableDevices returns customer devices for the Emporia account
 func getAvailableDevices(token string) []EmporiaDevice {
 	EmporiaDeviceURL := EmporiaBaseURL + "/customers/devices"
@@ -158,4 +142,20 @@ func getAvailableDevices(token string) []EmporiaDevice {
 	}
 
 	return devs.Devices
+}
+
+// EmporiaStatus returns if the Emporia API is available
+func EmporiaStatus() (bool, error) {
+
+	// https://github.com/magico13/PyEmVue/blob/master/api_docs.md#detection-of-maintenance
+	EmporiaStatusURL := "https://s3.amazonaws.com/com.emporiaenergy.manual.ota/maintenance/maintenance.json"
+
+	resp, err := http.Get(EmporiaStatusURL)
+	if err != nil {
+		return false, err
+	}
+	defer resp.Body.Close()
+
+	status := resp.StatusCode == 403
+	return status, nil
 }
