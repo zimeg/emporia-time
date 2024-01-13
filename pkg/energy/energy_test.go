748eb31487435ba10c6b5e7dfc59b774bd566b75
Ethan Zimbelman <ethan.zimbelman@me.com>
2024-01-13 13:31:15 -0800

    test: perform name tests and assert results

--- pkg/energy/energy_test.go
+++ pkg/energy/energy_test.go
@@ -3,119 +3,85 @@ package energy
 import (
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestScaleKWhToWs(t *testing.T) {
-	tests := []struct {
-		Title      string
+	tests := map[string]struct {
 		KWh        float64
 		ExpectedWs float64
 	}{
-		{
-			"ensure the zero value is zero",
+		"ensure the zero value is zero": {
 			0,
 			0,
 		},
-		{
-			"convert a single KWh to Ws",
+		"convert a single KWh to Ws": {
 			1,
 			1 * 1000 * 3600,
 		},
-		{
-			"convert 1000 KWh to Ws",
+		"convert 1000 KWh to Ws": {
 			1000,
 			1000 * 1000 * 3600,
 		},
 	}
-
-	for _, tt := range tests {
-		actual := ScaleKWhToWs(tt.KWh)
-		if tt.ExpectedWs != actual {
-			t.Fatalf("An unexpected energy conversion was found!\nTEST: '%s'\nEXPECT: %8f\nACTUAL: %8f",
-				tt.Title,
-				tt.ExpectedWs,
-				actual,
-			)
-		}
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			actual := ScaleKWhToWs(tt.KWh)
+			assert.Equal(t, tt.ExpectedWs, actual)
+		})
 	}
 }
 
 func TestExtrapolateUsage(t *testing.T) {
-	tests := []struct {
-		Title          string
+	tests := map[string]struct {
 		Measurements   []float64
 		Duration       time.Duration
 		ExpectedResult EnergyResult
 	}{
-		{
-			"handle the measurements of instant commands",
+		"handle the measurements of instant commands": {
 			[]float64{0},
 			time.Duration(0 * float64(time.Second)),
 			EnergyResult{Joules: 0, Watts: 0, Sureness: 1},
 		},
-		{
-			"return unsure results if no measurements are returned",
+		"return unsure results if no measurements are returned": {
 			[]float64{},
 			time.Duration(3 * float64(time.Second)),
 			EnergyResult{Joules: 0, Watts: 0, Sureness: 0},
 		},
-		{
-			"return unsure results if all measurements are zero",
+		"return unsure results if all measurements are zero": {
 			[]float64{0, 0, 0},
 			time.Duration(3 * float64(time.Second)),
 			EnergyResult{Joules: 0, Watts: 0, Sureness: 0},
 		},
-		{
-			"confidently compute results for complete measurements",
+		"confidently compute results for complete measurements": {
 			[]float64{3.64, 4.2, 2}, // sum=9.84, avg=3.28
 			time.Duration(3 * float64(time.Second)),
 			EnergyResult{Joules: 9.84, Watts: 3.28, Sureness: 1},
 		},
-		{
-			"extrapolate a missing second of measured results",
+		"extrapolate a missing second of measured results": {
 			[]float64{3, 4, 6, 3}, // sum=16, avg=4
 			time.Duration(5 * float64(time.Second)),
 			EnergyResult{Joules: 20, Watts: 4, Sureness: 0.8},
 		},
-		{
-			"extrapolate a half second of undermeasured results",
+		"extrapolate a half second of undermeasured results": {
 			[]float64{3, 4, 6, 3}, // sum=16, avg=4
 			time.Duration(4.5 * float64(time.Second)),
 			EnergyResult{Joules: 18, Watts: 4, Sureness: 4 / 4.5},
 		},
-		{
-			"extrapolate a half second of overmeasured results",
+		"extrapolate a half second of overmeasured results": {
 			[]float64{3, 4, 6, 3, 4}, // sum=20, avg=4
 			time.Duration(4.5 * float64(time.Second)),
 			EnergyResult{Joules: 18, Watts: 4, Sureness: 1},
 		},
 	}
-
-	for _, tt := range tests {
-		actual := ExtrapolateUsage(EnergyMeasurement{
-			Chart:    tt.Measurements,
-			Duration: tt.Duration,
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			measurement := EnergyMeasurement{Chart: tt.Measurements, Duration: tt.Duration}
+			actual := ExtrapolateUsage(measurement)
+			assert.Equal(t, tt.ExpectedResult.Joules, actual.Joules)
+			assert.Equal(t, tt.ExpectedResult.Watts, actual.Watts)
+			assert.Equal(t, tt.ExpectedResult.Sureness, actual.Sureness)
 		})
-		if tt.ExpectedResult.Joules != actual.Joules {
-			t.Fatalf("An unexpected joule estimation was found!\nFAILED: '%s'\nEXPECT: %8f\nACTUAL: %8f",
-				tt.Title,
-				tt.ExpectedResult.Joules,
-				actual.Joules,
-			)
-		}
-		if tt.ExpectedResult.Watts != actual.Watts {
-			t.Fatalf("An unexpected watt estimation was found!\nFAILED: '%s'\nEXPECT: %8f\nACTUAL: %8f",
-				tt.Title,
-				tt.ExpectedResult.Watts,
-				actual.Watts,
-			)
-		}
-		if tt.ExpectedResult.Sureness != actual.Sureness {
-			t.Fatalf("An unexpected sureness score was found!\nFAILED: '%s'\nEXPECT: %8f\nACTUAL: %8f",
-				tt.Title,
-				tt.ExpectedResult.Sureness,
-				actual.Sureness,
-			)
-		}
 	}
 }
--- pkg/times/time_test.go
+++ pkg/times/time_test.go
@@ -4,18 +4,18 @@ import (
 	"bytes"
 	"strings"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestParseTimeResults(t *testing.T) {
-	tests := []struct {
-		Title     string
+	tests := map[string]struct {
 		Output    []string
 		Times     CommandTime
 		CmdOutput []string
 		Error     error
 	}{
-		{
-			"check a command like sleep without any outputs",
+		"check a command like sleep without any outputs": {
 			[]string{
 				"real 1.00",
 				"user 1.00",
@@ -29,8 +29,7 @@ func TestParseTimeResults(t *testing.T) {
 			[]string{},
 			nil,
 		},
-		{
-			"capture and remove time values from the output",
+		"capture and remove time values from the output": {
 			[]string{
 				"example command output!",
 				"real 3754.56",
@@ -47,8 +46,7 @@ func TestParseTimeResults(t *testing.T) {
 			},
 			nil,
 		},
-		{
-			"prefer the latest outputs for timing information",
+		"prefer the latest outputs for timing information": {
 			[]string{
 				"this command outputs something familiar",
 				"real 3.00",
@@ -75,8 +73,7 @@ func TestParseTimeResults(t *testing.T) {
 			},
 			nil,
 		},
-		{
-			"ensure the parser doesnt break on unexpected values",
+		"ensure the parser doesnt break on unexpected values": {
 			[]string{
 				"gathering example account information",
 				"user goatish_burr",
@@ -100,79 +97,52 @@ func TestParseTimeResults(t *testing.T) {
 			nil,
 		},
 	}
-
-	for _, tt := range tests {
-		buff := bytes.NewBufferString(strings.Join(tt.Output, "\n"))
-		times, cmd, err := parseTimeResults(*buff)
-		stderr := bytes.NewBufferString(strings.Join(tt.CmdOutput, "\n"))
-
-		if tt.Error != err {
-			if tt.Error != nil && err != nil && tt.Error.Error() != err.Error() {
-				t.Fatalf("An unexpected error was encountered!\nTEST: '%s'\nEXPECT: %+v\nACTUAL: %+v",
-					tt.Title,
-					tt.Error,
-					err,
-				)
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			buff := bytes.NewBufferString(strings.Join(tt.Output, "\n"))
+			stdout := bytes.NewBufferString(strings.Join(tt.CmdOutput, "\n"))
+			times, cmd, err := parseTimeResults(*buff)
+			if tt.Error != nil {
+				assert.Error(t, err)
+				assert.Equal(t, tt.Error, err)
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, tt.Times, times)
+				if len(stdout.Bytes()) != 0 {
+					assert.Equal(t, stdout.Bytes(), cmd.Bytes())
+				} else {
+					assert.Nil(t, cmd.Bytes())
+				}
 			}
-		}
-		if tt.Times != times {
-			t.Fatalf("Not all times were collected!\nTEST: '%s'\nEXPECT: %+v\nACTUAL: %+v",
-				tt.Title,
-				tt.Times,
-				times,
-			)
-		}
-		if !bytes.Equal(stderr.Bytes(), cmd.Bytes()) {
-			t.Fatalf("The correct command output was not retained!\nTEST: '%s'\nEXPECT: %+v\nACTUAL: %+v",
-				tt.Title,
-				stderr.String(),
-				cmd.String(),
-			)
-		}
+		})
 	}
 }
 
 func TestParseTimeValue(t *testing.T) {
-	tests := []struct {
-		Title    string
+	tests := map[string]struct {
 		Value    []string
 		Expected []float64
 	}{
-		{
-			"retain the necessary leading zero",
+		"retain the necessary leading zero": {
 			[]string{"0.00", "0.08"},
 			[]float64{0.0, 0.08},
 		},
-		{
-			"ignore padding on seconds as needed",
+		"ignore padding on seconds as needed": {
 			[]string{"2.00", "4.00", "6.02", "12.80", "6.10"},
 			[]float64{2.0, 4.0, 6.02, 12.8, 6.1},
 		},
-		{
-			"parse times greater than sixty seconds",
+		"parse times greater than sixty seconds": {
 			[]string{"573.66", "261.01", "850.70", "86405.12"},
 			[]float64{573.66, 261.01, 850.7, 86405.12},
 		},
 	}
-
-	for _, tt := range tests {
-		for ii, val := range tt.Value {
-			trimmed, err := parseTimeValue(val)
-			if err != nil {
-				t.Fatalf("An unexpected error was found!\nTEST: '%s'\nEXPECT: %+v\nACTUAL: %+v",
-					tt.Title,
-					nil,
-					err,
-				)
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			for ii, val := range tt.Value {
+				trimmed, err := parseTimeValue(val)
+				assert.NoError(t, err)
+				assert.Equal(t, tt.Expected[ii], trimmed)
 			}
-			if trimmed != tt.Expected[ii] {
-				t.Fatalf("The formatting of time seems off for '%s'!\nTEST: '%s'\nEXPECT: %+v\nACTUAL: %+v",
-					val,
-					tt.Title,
-					tt.Expected[ii],
-					trimmed,
-				)
-			}
-		}
+		})
 	}
 }
