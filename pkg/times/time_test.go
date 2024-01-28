baf4a14263be935842ab14149ffa200643cf3434
Ethan Zimbelman <ethan.zimbelman@me.com>
2024-01-27 23:46:36 -0800

    test: remove cases from a specific implementation

--- pkg/times/time_test.go
+++ pkg/times/time_test.go
@@ -1,7 +1,6 @@
 package times
 
 import (
-	"bytes"
 	"strings"
 	"testing"
 
@@ -10,110 +9,30 @@ import (
 
 func TestParseTimeResults(t *testing.T) {
 	tests := map[string]struct {
-		Output    []string
-		Times     CommandTime
-		CmdOutput []string
-		Error     error
+		Output []string
+		Times  CommandTime
+		Error  error
 	}{
-		"check a command like sleep without any outputs": {
+		"parse the portable output of the time command": {
 			[]string{
-				"real 1.00",
-				"user 1.00",
-				"sys 1.00",
+				"real 6.00",
+				"user 2.20",
+				"sys 3.80",
 			},
 			CommandTime{
-				Real: 1.0,
-				User: 1.0,
-				Sys:  1.0,
-			},
-			[]string{},
-			nil,
-		},
-		"capture and remove time values from the output": {
-			[]string{
-				"example command output!",
-				"real 3754.56",
-				"user 1301.21",
-				"sys 3.24",
-			},
-			CommandTime{
-				Real: 3754.56,
-				User: 1301.21,
-				Sys:  3.24,
-			},
-			[]string{
-				"example command output!",
-			},
-			nil,
-		},
-		"prefer the latest outputs for timing information": {
-			[]string{
-				"this command outputs something familiar",
-				"real 3.00",
-				"user 3.00",
-				"sys 3.00",
-				"but suppose that was hardcoded!",
-				"the real output now follows",
-				"real 240.00",
-				"user 8.00",
-				"sys 12.00",
-			},
-			CommandTime{
-				Real: 240.0,
-				User: 8.0,
-				Sys:  12.0,
-			},
-			[]string{
-				"this command outputs something familiar",
-				"real 3.00",
-				"user 3.00",
-				"sys 3.00",
-				"but suppose that was hardcoded!",
-				"the real output now follows",
-			},
-			nil,
-		},
-		"ensure the parser doesnt break on unexpected values": {
-			[]string{
-				"gathering example account information",
-				"user goatish_burr",
-				"real true",
-				"sys mountains",
-				"real 240.00",
-				"user 8.05",
-				"sys 12.00",
-			},
-			CommandTime{
-				Real: 240.0,
-				User: 8.05,
-				Sys:  12.0,
-			},
-			[]string{
-				"gathering example account information",
-				"user goatish_burr",
-				"real true",
-				"sys mountains",
+				Real: 6.0,
+				User: 2.2,
+				Sys:  3.8,
 			},
 			nil,
 		},
 	}
 	for name, tt := range tests {
 		t.Run(name, func(t *testing.T) {
-			buff := bytes.NewBufferString(strings.Join(tt.Output, "\n"))
-			stdout := bytes.NewBufferString(strings.Join(tt.CmdOutput, "\n"))
-			times, cmd, err := parseTimeResults(*buff)
-			if tt.Error != nil {
-				assert.Error(t, err)
-				assert.Equal(t, tt.Error, err)
-			} else {
-				assert.NoError(t, err)
-				assert.Equal(t, tt.Times, times)
-				if len(stdout.Bytes()) != 0 {
-					assert.Equal(t, stdout.Bytes(), cmd.Bytes())
-				} else {
-					assert.Nil(t, cmd.Bytes())
-				}
-			}
+			output := strings.Join(tt.Output, "\n")
+			times, err := parseTimeResults(output)
+			assert.Equal(t, tt.Error, err)
+			assert.Equal(t, tt.Times, times)
 		})
 	}
 }
