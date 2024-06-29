528281231e1e1bd61917ebc6acdc47fe86b9b4bb
@zimeg <zim@o526.net>
2024-06-28 18:38:17 -0700

    build(flake): package the gon tool from an upstream nur package

--- flake.nix
+++ flake.nix
@@ -2,24 +2,17 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    zimeg.url = "github:zimeg/nur-packages";
   };
-  outputs = { nixpkgs, flake-utils, ... }:
+  outputs = { nixpkgs, flake-utils, zimeg, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        gon = if system == "x86_64-darwin" || system == "aarch64-darwin" then
-          let
-            gonZip = pkgs.fetchurl {
-              url = "https://github.com/Bearer/gon/releases/download/v0.0.36/gon_macos.zip";
-              sha256 = "1firj23pgdfx9hybjjr91chn1jzf7lzjrbx3nm1s9h3xbpx945x2";
-            };
-          in
-          pkgs.runCommand "gon" { nativeBuildInputs = [ pkgs.unzip ]; } ''
-            mkdir -p $out/bin
-            unzip ${gonZip} -d $out/bin
-          ''
-        else
-          null;
+        gon =
+          if system == "x86_64-darwin" || system == "aarch64-darwin" then
+            zimeg.packages.${pkgs.system}.gon
+          else
+            null;
       in
       {
         devShells.default = pkgs.mkShell {
@@ -29,23 +22,11 @@
             gocyclo
             gofumpt
             golangci-lint
+            gon
             gopls
             goreleaser
           ];
           shellHook = "go mod tidy";
         };
-        devShells.gon = if system == "x86_64-darwin" || system == "aarch64-darwin" then
-          pkgs.mkShell {
-            buildInputs = with pkgs; [
-              go
-              gon
-              goreleaser
-            ];
-            shellHook = ''
-              export PATH=/usr/bin:$PATH:${gon}/bin
-            '';
-          }
-        else
-          null;
       });
 }
