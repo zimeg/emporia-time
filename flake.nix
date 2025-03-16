857ddc5bb36a325c5dc352e15cb9b74407484bdf
Eden Zimbelman <zim@o526.net>
2025-03-16 10:21:16 -0700

    build: package the build in a flake and test with propogated timing (#149)

--- flake.nix
+++ flake.nix
@@ -4,15 +4,13 @@
     flake-utils.url = "github:numtide/flake-utils";
     zimeg.url = "github:zimeg/nur-packages";
   };
-  outputs = { nixpkgs, flake-utils, ... } @ inputs:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    { nixpkgs, flake-utils, ... }@inputs:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        gon =
-          if pkgs.stdenv.isDarwin then
-            inputs.zimeg.packages.${pkgs.system}.gon
-          else
-            null;
+        gon = if pkgs.stdenv.isDarwin then inputs.zimeg.packages.${pkgs.system}.gon else null;
       in
       {
         devShells.default = pkgs.mkShell {
@@ -31,18 +29,40 @@
         };
         devShells.gon =
           if pkgs.stdenv.isDarwin then
-            pkgs.mkShell
-              {
-                packages = with pkgs; [
-                  go
-                  gon
-                  goreleaser
-                ];
-                shellHook = ''
-                  export PATH=/usr/bin:$PATH # https://github.com/zimeg/nur-packages/issues/4
-                '';
-              }
+            pkgs.mkShell {
+              packages = with pkgs; [
+                go
+                gon
+                goreleaser
+              ];
+              shellHook = ''
+                export PATH=/usr/bin:$PATH # https://github.com/zimeg/nur-packages/issues/4
+              '';
+            }
           else
             null;
-      });
+        devShells.tom = pkgs.mkShell {
+          packages = with pkgs; [
+            time
+          ];
+        };
+        packages.default = pkgs.buildGoModule {
+          pname = "emporia-time";
+          version = "unversioned";
+          src = ./.;
+          ldflags = [
+            "-s"
+            "-w"
+            "-X main.version=dev"
+          ];
+          doCheck = true;
+          vendorHash = "sha256-G5sLF3awEQGkSqaXmhSw+IcBq+NoG3QoR+L8XymWfDU=";
+          meta = {
+            description = "an energy aware time command";
+            homepage = "https://github.com/zimeg/emporia-time";
+            license = pkgs.lib.licenses.mit;
+          };
+        };
+      }
+    );
 }
