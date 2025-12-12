cec31c68143ac43d5935f2824024a332601455aa
@zimeg <zim@o526.net>
2025-12-11 18:09:36 -0800

    ci: update indirect dependencies alongside latest package change

--- flake.nix
+++ flake.nix
@@ -56,7 +56,7 @@
             "-X main.version=${version}"
           ];
           doCheck = true;
-          vendorHash = "sha256-PcrIUORASv66i7vVhESXogvFlU2xPPMCu7cmqJPasoc=";
+          vendorHash = "sha256-hGC31CjRg2oRovzz9cJjdb57DbZ7MfgEBnGz0Lg3dCk=";
           nativeBuildInputs = [
             pkgs.installShellFiles
           ];
